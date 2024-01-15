9d750c34c8e16cf0d50b1ec11e15b00e288c84fb
g.trapeau <g.trapeau@lyon.ort.asso.fr>
2024-01-15 14:32:06 +0100

    Updating config folder

---
+++ config/db.php
@@ -0,0 +1,34 @@
+<?php
+
+function dbConnect()
+{
+    try {
+        $infosConnexion = parse_ini_file(__DIR__.'/parameters.ini');
+        // Information de la BDD
+        $infoBdd = array(
+            'type' => 'mysql',
+            'host' => $infosConnexion['database_host'],
+            'port' => $infosConnexion['database_port'], // 5432 pour postgreSQL, 3306 pour MySQL
+            'charset' => 'UTF8',
+            'dbname' => $infosConnexion['database_name'],
+            'user' => $infosConnexion['database_user'],
+            'pass' => $infosConnexion['database_password'],
+        );
+
+        $hostname = $infoBdd['host'];
+        $mydbname = $infoBdd['dbname'];
+        $myusername = $infoBdd['user'];
+        $mypassword = $infoBdd['pass'];
+        $mydriver = $infoBdd['type'];
+        $myport = $infoBdd['port'];
+        $mycharset = $infoBdd['charset'];
+
+        // Connexion PDO
+        $db = new PDO("$mydriver:dbname=$mydbname;host=$hostname;port=$myport;options='--client_encoding=$mycharset'", $myusername, $mypassword, [PDO::MYSQL_ATTR_LOCAL_INFILE => true]);
+        $db->exec("SET NAMES 'UTF8'");
+
+        return $db;
+    } catch (Exception $e) {
+        exit('Erreur : '.$e->getMessage());
+    }
+}
