c5eda0f638b1a60afeabd0ba4d4a844db6719132
SkyWors <skyworslegrand@gmail.com>
2024-01-24 21:05:29 +0100

    Rework logger and reimplement it

--- config/import.php
+++ config/import.php
@@ -2,9 +2,10 @@
 	// Start session
 	session_start();
 
+	// Import database
+	require $_SERVER['DOCUMENT_ROOT'] . "/config/db.php";
+
 	// Import logger
 	require($_SERVER['DOCUMENT_ROOT'] . "/src/App/Repositories/log.php");
 
-	// Import database
-	require $_SERVER['DOCUMENT_ROOT'] . "/config/db.php";
 ?>
