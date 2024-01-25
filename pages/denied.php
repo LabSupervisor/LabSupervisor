ad7c2e9cbc3e4f7980215cf2d868f4491c30024b
SkyWors <skyworslegrand@gmail.com>
2024-01-25 14:31:30 +0100

    Add small security to all directories, add denied page

---
+++ pages/denied.php
@@ -0,0 +1,10 @@
+<?php
+	require($_SERVER["DOCUMENT_ROOT"] . "/logic/ft_header.php");
+	mainHeader("Création de compte");
+?>
+
+<h1>Access denied.</h1>
+
+<a href="<?="http://" . $_SERVER["SERVER_NAME"]?>">
+	<button>Retourner à l'accueil</button>
+</a>
