4e3667207d6e9dbc260c396a5592b4831bbc1bdf
marine404 <marine.gonnord.pro@gmail.com>
2024-01-26 17:06:02 +0100

    drop down menu in progress

--- pages/sessioncreation.php
+++ pages/sessioncreation.php
@@ -20,8 +20,29 @@
 		<legend> Participants </legend>
 		<strong> Classes : </strong>
 		<!-- TODO Request get class -->
+		<?php
+		$db = dbConnect();
+
+		$queryClass = "SELECT id, name FROM classroom";
+		$queryClassPrep = $db->prepare($queryClass);
+		if ($queryClassPrep->execute()) {
+			// Récupérer la colonne 'id' en tant que chaîne de caractères
+			$tabClass = $queryClassPrep->fetchAll();
+
+
+			// foreach($tabClass as $line){
+			// 	// var_dump($line);
+			// 	// echo $line["id"];
+			// 	echo $line["name"]. " ";
+			// }
+		}
+
+
+
+		?>
+
 		<select name="classes" id="classes">
-			<option value="classe1">Classe 1</option>
+			<option value="classe1"> <?php echo $tabClass[1]["name"] ?></option>
 			<option value="classe2">Classe 2</option>
 			<option value="classe3">Classe 3</option>
 		</select>
