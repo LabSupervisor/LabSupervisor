b007b35b010768c7afa5b19e6851526a8a6eb0c7
marine404 <marine.gonnord.pro@gmail.com>
2024-01-26 18:16:55 +0100

    Get classroom to display it on sessioncreation's menu OK

--- pages/sessioncreation.php
+++ pages/sessioncreation.php
@@ -19,33 +19,27 @@
 	<fieldset>
 		<legend> Participants </legend>
 		<strong> Classes : </strong>
-		<!-- TODO Request get class -->
+
 		<?php
 		$db = dbConnect();
 
 		$queryClass = "SELECT id, name FROM classroom";
 		$queryClassPrep = $db->prepare($queryClass);
 		if ($queryClassPrep->execute()) {
-			// Récupérer la colonne 'id' en tant que chaîne de caractères
 			$tabClass = $queryClassPrep->fetchAll();
-
-
-			// foreach($tabClass as $line){
-			// 	// var_dump($line);
-			// 	// echo $line["id"];
-			// 	echo $line["name"]. " ";
-			// }
 		}
-
-
-
 		?>
 
 		<select name="classes" id="classes">
-			<option value="classe1"> <?php echo $tabClass[1]["name"] ?></option>
-			<option value="classe2">Classe 2</option>
-			<option value="classe3">Classe 3</option>
+			<?php
+				for ($i = 0; $i< count($tabClass); $i++){
+				?>
+					<option value="classe"> <?php echo $tabClass[$i]["name"] ?></option>
+				<?php
+				}
+			?>
 		</select>
+
 	</fieldset>
 	<fieldset>
 		<legend> Chapitres </legend>
