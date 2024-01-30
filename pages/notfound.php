805e56d3d405c913e6b4b01b69c050a6ea9bd95b
SkyWors <skyworslegrand@gmail.com>
2024-01-30 15:26:47 +0100

    Add first version htaccess file

--- pages/notfound.php
+++ pages/notfound.php
@@ -1,9 +1,9 @@
 <?php
 	require($_SERVER["DOCUMENT_ROOT"] . "/logic/ft_header.php");
-	mainHeader("Accès refusé");
+	mainHeader("Page non trouvé");
 ?>
 
-<h1>Access denied.</h1>
+<h1>404 not found</h1>
 
 <a href="<?="http://" . $_SERVER["SERVER_NAME"]?>">
 	<button>Retourner à l'accueil</button>
