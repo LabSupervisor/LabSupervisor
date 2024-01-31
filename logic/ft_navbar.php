94c8e14f53ed4e4ca1039eb68446c3586f8cda82
SkyWors <skyworslegrand@gmail.com>
2024-01-31 22:42:45 +0100

    Temporarily fix compatibility error

--- logic/ft_navbar.php
+++ logic/ft_navbar.php
@@ -16,7 +16,7 @@
 			// Check if user is connected
 			if (!isset($_SESSION["login"])) { ?>
 					<li>
-						<a class="title bold" href="<?="http://" . $_SERVER["SERVER_NAME"] . "/login"?>"><i class="ri-user-line"></i> Connexion</a>
+						<a class="title bold" href="<?="http://" . $_SERVER["SERVER_NAME"] . "/login.php"?>"><i class="ri-user-line"></i> Connexion</a>
 					</li>
 				</ul>
 			</nav>
@@ -30,16 +30,16 @@
 						<a class="title bold" href="#"><i class="ri-folder-line"></i> Classes</a>
 					</li>
 					<li>
-						<a class="title bold" href="<?="http://" . $_SERVER["SERVER_NAME"] . "/sessioncreation"?>"><i class="ri-computer-line"></i> Créer une session</a>
+						<a class="title bold" href="<?="http://" . $_SERVER["SERVER_NAME"] . "/sessioncreation.php"?>"><i class="ri-computer-line"></i> Créer une session</a>
 					</li>
 					<li>
-						<a class="title bold" href="<?="http://" . $_SERVER["SERVER_NAME"] . "/session"?>"><i class="ri-slideshow-3-line"></i> Voir mes sessions</a>
+						<a class="title bold" href="<?="http://" . $_SERVER["SERVER_NAME"] . "/session.php"?>"><i class="ri-slideshow-3-line"></i> Voir mes sessions</a>
 					</li>
 		<?php }
 			// If the user is a student
 			else if(in_array("student", $roleList)) { ?>
 				<li>
-					<a class="title bold" href="<?="http://" . $_SERVER["SERVER_NAME"] . "/session"?>"><i class="ri-slideshow-3-line"></i> Voir mes sessions</a>
+					<a class="title bold" href="<?="http://" . $_SERVER["SERVER_NAME"] . "/session.php"?>"><i class="ri-slideshow-3-line"></i> Voir mes sessions</a>
 				</li>
 		<?php }
 			// If the user is an admin
@@ -51,7 +51,7 @@
 					<a class="title bold" href="#"><i class="ri-slideshow-3-line"></i> Sessions</a>
 				</li>
 				<li>
-					<a class="title bold" href="<?="http://" . $_SERVER["SERVER_NAME"] . "/log?trace"?>"><i class="ri-computer-line"></i> Logs</a>
+					<a class="title bold" href="<?="http://" . $_SERVER["SERVER_NAME"] . "/log.php?trace"?>"><i class="ri-computer-line"></i> Logs</a>
 				</li>
 		<?php
 			}
@@ -63,13 +63,13 @@
 			<li><a class="title bold case" href="#"><i class="ri-user-line"></i> <?=$username?></a>
 				<ul class="sub">
 					<li>
-						<a class="title bold" href="<?="http://" . $_SERVER["SERVER_NAME"] . "/account"?>"><i class="ri-account-circle-line"></i> Compte</a>
+						<a class="title bold" href="<?="http://" . $_SERVER["SERVER_NAME"] . "/account.php"?>"><i class="ri-account-circle-line"></i> Compte</a>
 					</li>
 					<li>
-						<a class="title bold" href="<?="http://" . $_SERVER["SERVER_NAME"] . "/setting"?>"><i class="ri-settings-4-line"></i> Options</a>
+						<a class="title bold" href="<?="http://" . $_SERVER["SERVER_NAME"] . "/setting.php"?>"><i class="ri-settings-4-line"></i> Options</a>
 					</li>
 					<li>
-						<a class="title bold" href="<?="http://" . $_SERVER["SERVER_NAME"] . "/logic/disconnect"?>"><i class="ri-logout-box-line"></i> Deconnexion</a>
+						<a class="title bold" href="<?="http://" . $_SERVER["SERVER_NAME"] . "/logic/disconnect.php"?>"><i class="ri-logout-box-line"></i> Deconnexion</a>
 					</li>
 				</ul>
 				</li>
