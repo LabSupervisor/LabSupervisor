f78b758a6ada80a5d3957fdfb4ef547b84b2a19c
SkyWors <skyworslegrand@gmail.com>
2024-02-09 19:35:11 +0100

    Format user administration

---
+++ logic/ft_roleFormat.php
@@ -0,0 +1,13 @@
+<?php
+	function roleFormat($student, $teacher, $admin) {
+		$result = "";
+		if ($student == True)
+			$result = $result . "Etudiant";
+		if ($teacher == True)
+			$result = $result . "Enseignant";
+		if ($admin == True)
+			$result = $result . "Admin";
+
+		return $result;
+	}
+?>
