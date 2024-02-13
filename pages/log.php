7b1cd9493fe1e256a85ccd7b2249ce3fd15c16ed
SkyWors <skyworslegrand@gmail.com>
2024-02-13 14:50:40 +0100

    Fix log display

--- pages/log.php
+++ pages/log.php
@@ -16,132 +16,146 @@
 <?php
 	if (isset($_GET["trace"])) {
 ?>
-	<div class="table-container">
-		<table>
-			<thead>
-				<tr>
-					<td>ID</td>
-					<td>Utilisateur</td>
-					<td>Message</td>
-					<td>Date</td>
-				</tr>
-			</thead>
-			<tbody>
-				<?php
-				$logs = getDBLog();
+<div class="table-container">
+	<table>
+		<thead>
+			<tr>
+				<td>ID</td>
+				<td>Utilisateur</td>
+				<td>Message</td>
+				<td>Date</td>
+			</tr>
+		</thead>
+		<tbody>
+			<?php
+			$logs = getDBLog();
 
-				foreach ($logs as $line) {
-					echo "<tr>";
-					echo '<th class="col1">' . $line["id"] . '</th>';
-					echo '<td class="col2">' . getName($line["iduser"]) . '</td>';
-					echo '<td class="col3-container">';
-					echo '<div class="col3-tooltip">' . htmlspecialchars($line["message"]) . '</div>';
-					echo '<div class="col3">' . $line["message"] . '</div>';
-					echo '</td>';
-					echo '<td class="col4">' . $line["date"] . '</td>';
-					echo "</tr>";
-				}
-				?>
-			</tbody>
-		</table>
+			foreach ($logs as $line) {
+				echo "<tr>";
+				echo '<th class="col1">' . $line["id"] . '</th>';
+				echo '<td class="col2">' . getName($line["iduser"]) . '</td>';
+				echo '<td class="col3-container">';
+				echo '<div class="col3-tooltip">' . htmlspecialchars($line["message"]) . '</div>';
+				echo '<div class="col3">' . $line["message"] . '</div>';
+				echo '</td>';
+				echo '<td class="col4">' . $line["date"] . '</td>';
+				echo "</tr>";
+			}
+			?>
+		</tbody>
+	</table>
 
-		<div class="button-container">
-			<a href="/log?trace">
-				<button><i class="ri-draft-line"></i> Trace</button>
-			</a>
-			<a href="/log?error">
-				<button><i class="ri-error-warning-line"></i> Erreur</button>
-			</a>
-		</div>
+	<div class="button-container">
+		<a href="/log?trace">
+			<button><i class="ri-draft-line"></i> Trace</button>
+		</a>
+		<a href="/log?error">
+			<button><i class="ri-error-warning-line"></i> Erreur</button>
+		</a>
 	</div>
+</div>
 <?php
 } else if (isset($_GET["error"])) {
 	if (!isset($_GET["date"])) {
-		$date = date("Y-m-d");
+		$fileDate = date("Y-m-d");
 	} else {
-		$date = $_GET["date"];
+		$fileDate = $_GET["date"];
 	}
 
-	$file = $_SERVER["DOCUMENT_ROOT"] . "/logs/" . $date . ".log";
+	$file = $_SERVER["DOCUMENT_ROOT"] . "/logs/" . $fileDate . ".log";
 	if (file_exists($file)) {
 		$logs = file_get_contents($file);
-		$logs = explode("\n", $logs);
 ?>
-	<div class="table-container2">
-		<table>
-			<thead>
-				<tr>
-					<th>Erreur</th>
-					<th>Message</th>
-					<th>Date</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php
-				foreach ($logs as $line) {
-					$date = "";
-					$error = "";
-					$message = "";
-					$datePattern = "/\[(.*?)\]/";
-					$errorPattern = "/\](.*?)\:/";
-					$messagePattern = "/: (.*)$/";
 
-					if (preg_match($datePattern, $line, $matches))
-						$date = $matches[1];
-					if (preg_match($errorPattern, $line, $matches))
-						$error = $matches[1];
-					if (preg_match($messagePattern, $line, $matches))
-						$message = $matches[1];
-
-					echo "<tr>";
-					echo "<td class='col1'>" . $error . "</td>";
-					echo "<td class='col3-container'>";
-					echo '<div class="col3-tooltip">' . htmlspecialchars($message) . '</div>';
-					echo '<div class="col3">' . $message . '</div>';
-					echo "</td>";
-					echo "<td class='col2'>" . $date . "</td>";
-					echo "</tr>";
+<div class="table-container2">
+	<table>
+		<thead>
+			<tr>
+				<th>Erreur</th>
+				<th>Message</th>
+				<th>Heure</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php
+				$listDate = array();
+				$temp = explode("\n", $logs);
+				foreach($temp as $value) {
+					if (preg_match("/\[" . $fileDate . " (.*?)\]/", $value, $matches)) {
+						array_push($listDate, $matches[1]);
+					}
 				}
-				?>
-			</tbody>
-		</table>
 
-		<div class="button-container">
-			<a href="/log?trace">
-				<button><i class="ri-draft-line"></i> Trace</button>
-			</a>
-			<a href="/log?error">
-				<button><i class="ri-error-warning-line"></i> Erreur</button>
-			</a>
-			<div class="right-buttons">
-				<form method="get">
-					<input type="hidden" name="error">
-					<input type="date" name="date">
-					<input type="submit" value="Envoyer">
-				</form>
-			</div>
+				$log = preg_split("/\[" . $fileDate . " (.*?)\]/", $logs);
+
+				$i = -1;
+				foreach($log as $temp) {
+					$message = "";
+					$error = "";
+
+					$data = explode("\n", $temp);
+					foreach($data as $value) {
+						if (!str_starts_with($value, "#")) {
+							if (strlen($value) > 1) {
+								$error = $value;
+							}
+						} else {
+							$message .= $value . " ";
+						}
+					}
+
+					if (strlen($error) > 1) {
+						echo "<tr>";
+						echo "<td class='col1'>" . $error . "</td>";
+						echo "<td class='col3-container'>";
+						echo "<div class='col3-tooltip'>" . htmlspecialchars(str_replace("##", "#", $message)) . "</div>";
+						echo "<div class='col3'>" . str_replace("##", "#", $message) . "</div>";
+						echo "</td>";
+						echo "<td class='col2'>" . $listDate[$i] . "</td>";
+						echo "</tr>";
+					}
+					$i++;
+				}
+			?>
+		</tbody>
+	</table>
+
+	<div class="button-container">
+		<a href="/log?trace">
+			<button><i class="ri-draft-line"></i> Trace</button>
+		</a>
+		<a href="/log?error">
+			<button><i class="ri-error-warning-line"></i> Erreur</button>
+		</a>
+		<div class="right-buttons">
+			<form method="get">
+				<input type="hidden" name="error">
+				<input type="date" name="date">
+				<input type="submit" value="Envoyer">
+			</form>
 		</div>
 	</div>
+</div>
 
-		<?php
+	<?php
 	} else {
 		echo "Fichier introuvable"; ?>
 
-		<div class="button-container">
-		<a href="/log?trace">
-			 <button><i class="ri-draft-line"></i> Trace</button>
-		 </a>
-		 <a href="/log?error">
-			 <button><i class="ri-error-warning-line"></i> Erreur</button>
-		 </a>
-		 <div class="right-buttons">
-			 <form method="get">
-				 <input type="hidden" name="error">
-				 <input type="date" name="date">
-				 <input type="submit" value="Envoyer">
-			 </form>
-		 </div>
-	 </div>
+	<div class="button-container">
+	<a href="/log?trace">
+			<button><i class="ri-draft-line"></i> Trace</button>
+		</a>
+		<a href="/log?error">
+			<button><i class="ri-error-warning-line"></i> Erreur</button>
+		</a>
+		<div class="right-buttons">
+			<form method="get">
+				<input type="hidden" name="error">
+				<input type="date" name="date">
+				<input type="submit" value="Envoyer">
+			</form>
+		</div>
+	</div>
 <?php
 	}
 }
