14e68c8437bb9edb50948683d16830dd1baab11a
SkyWors <skyworslegrand@gmail.com>
2024-03-08 14:45:52 +0100

    Fix navbar with multiple role

--- logic/ft_navbar.php
+++ logic/ft_navbar.php
@@ -8,8 +8,7 @@
 		<div class="logo-container left">
 			<?php
 				// Icon path
-				$cheminImage = "/public/img/logo.ico";
-				echo "<img src=" . $cheminImage . "></img>";
+				echo "<img src='/public/img/logo.ico'></img>";
 			?>
 		</div>
 			<a class="bold title left no-hover-color">LabSupervisor</a>
@@ -28,46 +27,31 @@
 		<?php
 			// If the user is connected
 			} else {
+				$navbarItem = "";
 				$roleList = permissionChecker(true, "");
-			// If the user is a teacher
-			if (in_array(teacher, $roleList)) { ?>
-					<li>
-						<a class="title" href="/classes"><i class="ri-folder-line"></i> Classes</a>
-					</li>
-					<li>
-						<a class="title" href="/sessioncreation"><i class="ri-computer-line"></i> Créer une session</a>
-					</li>
-					<li>
-						<a class="title" href="/sessions"><i class="ri-slideshow-3-line"></i> Voir mes sessions</a>
-					</li>
-		<?php }
-			// If the user is a student
-			else if(in_array(student, $roleList)) { ?>
-				<li>
-					<a class="title" href="/sessions"><i class="ri-slideshow-3-line"></i> Voir mes sessions</a>
-				</li>
-		<?php }
-			// If the user is an admin
-			else if (in_array(admin, $roleList)) { ?>
-				<li>
-					<a class="title" href="/utilisateurs"><i class="ri-folder-line"></i> Utlisateurs</a>
-				</li>
-				<li>
-					<a class="title" href="/classes"><i class="ri-folder-line"></i> Classes</a>
-				</li>
-				<li>
-					<a class="title" href="/sessions"><i class="ri-slideshow-3-line"></i> Sessions</a>
-				</li>
-				<li>
-					<a class="title" href="/logs?trace"><i class="ri-computer-line"></i> Logs</a>
-				</li>
-		<?php
-			}
-			// Profil part if connected
-		?>
-			<?php
+
+				if (in_array(admin, $roleList) || in_array(teacher, $roleList)) {
+					$navbarItem .= '<li><a class="title" href="/classes"><i class="ri-folder-line"></i> Classes</a></li>';
+				}
+
+				if (in_array(teacher, $roleList)) {
+					$navbarItem .= '<li><a class="title" href="/sessioncreation"><i class="ri-computer-line"></i> Créer une session</a></li>';
+				}
+
+				if (in_array(admin, $roleList) || in_array(student, $roleList) || in_array(teacher, $roleList)) {
+					$navbarItem .= '<li><a class="title" href="/sessions"><i class="ri-slideshow-3-line"></i> Voir mes sessions</a></li>';
+				}
+
+				if (in_array(admin, $roleList)) {
+					$navbarItem .= '<li><a class="title" href="/utilisateurs"><i class="ri-folder-line"></i> Utlisateurs</a></li>';
+					$navbarItem .= '<li><a class="title" href="/logs?trace"><i class="ri-computer-line"></i> Logs</a></li>';
+				}
+
+				echo $navbarItem;
+
+				// Profil part if connected
 				$username = nameFormat($_SESSION["login"], true);
-			?>
+		?>
 			<li><a class="title case profil" href="#"><i class="ri-user-line"></i> <?=$username?></a>
 				<ul class="sub">
 					<li>
