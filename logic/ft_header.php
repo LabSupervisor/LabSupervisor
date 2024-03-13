79c1b08209d4cd23e52e3cc4f21c639ee5343af9
SkyWors <skyworslegrand@gmail.com>
2024-03-13 21:20:11 +0100

    Optimize navbar and header

--- logic/ft_header.php
+++ logic/ft_header.php
@@ -1,43 +1,39 @@
 <?php
-	function mainHeader($title) {
-		// Import main functions
-		require($_SERVER["DOCUMENT_ROOT"] . "/config/config.php");
 
-		// Page title
-		if ($title != "")
-			$title = $title . " - " . lang("MAIN_TITLE");
-		else
-			$title = lang("MAIN_TITLE");
+function mainHeader($title) {
+	// Import main functions
+	require($_SERVER["DOCUMENT_ROOT"] . "/config/config.php");
 
-		// Select user color theme
-		if (isset($_SESSION["login"]))
-			if (UserRepository::getSetting($_SESSION["login"])["theme"] == 0)
-				$theme = "colorlight";
-			else
-				$theme = "colordark";
-		else
+	// Page title
+	if ($title != "")
+		$title = $title . " - " . lang("MAIN_TITLE");
+	else
+		$title = lang("MAIN_TITLE");
+
+	// Select user color theme
+	if (isset($_SESSION["login"]))
+		if (UserRepository::getSetting($_SESSION["login"])["theme"] == 0)
 			$theme = "colorlight";
+		else
+			$theme = "colordark";
+	else
+		$theme = "colorlight";
 
-		// Setup stylesheet path
-		$theme = "/public/css/$theme.css";
-		$main = "/public/css/main.css";
-		$navbar = "/public/css/navbar.css";
-		$remixicon = "/public/css/import/remixicon.css";
+	$header = "";
 
-		// Create header
-		echo <<<EOT
-			<!DOCTYPE html>
-			<html lang="fr">
-			<head>
-				<meta charset="UTF-8">
-				<title>$title</title>
-				<link rel="stylesheet" href="$theme">
-				<link rel="stylesheet" href="$main">
-				<link rel="stylesheet" href="$navbar">
-				<link rel="stylesheet" href="$remixicon">
-			</head>
-		EOT;
+	// Create header
+	$header .= '<!DOCTYPE html><html lang="fr"><head><meta charset="UTF-8">';
+	$header .= '<title>' . $title . '</title>';
 
-		// Import navbar
-		require($_SERVER["DOCUMENT_ROOT"] . "/logic/ft_navbar.php");
-	}
+	$header .= '<link rel="stylesheet" href="/public/css/' . $theme . '.css">';
+	$header .= '<link rel="stylesheet" href="/public/css/main.css">';
+	$header .= '<link rel="stylesheet" href="/public/css/navbar.css">';
+	$header .= '<link rel="stylesheet" href="/public/css/import/remixicon.css">';
+
+	$header .= '</head>';
+
+	echo $header;
+
+	// Import navbar
+	require($_SERVER["DOCUMENT_ROOT"] . "/logic/ft_navbar.php");
+}
--- logic/ft_navbar.php
+++ logic/ft_navbar.php
@@ -2,103 +2,84 @@
 	// Logic
 	require($_SERVER["DOCUMENT_ROOT"] . "/logic/ft_nameFormat.php");
 	require($_SERVER["DOCUMENT_ROOT"] . "/logic/updateTheme.php");
+
+	$navbarItem = "";
 ?>
 
 <body>
-	<nav class="navbar">
-		<li>
-			<a tabindex="-1" href="/" class="bold no-hover-color"><img class="icon" src='/public/img/logo.ico'></img><?= lang("NAVBAR_TITLE") ?></a>
-		</li>
-		<li>
-			<a href="/"><i class="ri-home-line"></i> <?= lang("NAVBAR_HOME") ?></a>
-		</li>
+<nav class="navbar">
+	<?php
+
+	// Icon
+	$navbarItem .= '<li><a tabindex="-1" href="/" class="bold no-hover-color"><img class="icon" src="/public/img/logo.ico"></img>' . lang("NAVBAR_TITLE") . '</a></li>';
+	$navbarItem .= '<li><a href="/"><i class="ri-home-line"></i>' . lang("NAVBAR_HOME") . '</a></li>';
+
+	// If the user is not connected
+	if (!isset($_SESSION["login"])) {
+		$navbarItem .= '<li><a href="/login"><i class="ri-user-line"></i>' . lang("NAVBAR_CONNECT") . '</a></li></nav>';
+
+	// If the user is connected
+	} else {
+		$roleList = permissionChecker(true, "");
+
+		// Get current user theme
+		$userSetting = UserRepository::getSetting($_SESSION["login"]);
+		if ($userSetting["theme"] == "0"){
+			$theme = "dark";
+			$icon = "<i class='ri-moon-line'></i>";
+		}
+		else {
+			$theme = "light";
+			$icon = "<i class='ri-sun-line'></i>";
+		}
+
+		// Teacher pages
+		if (in_array(TEACHER, $roleList)) {
+			$navbarItem .= '<li><a href="/classes"><i class="ri-folder-line"></i> ' . lang("NAVBAR_CLASS") . '</a></li>';
+			$navbarItem .= '<li><a href="/sessioncreation"><i class="ri-computer-line"></i> ' . lang("NAVBAR_CREATE_SESSION") . '</a></li>';
+		}
+
+		// Global pages
+		if (in_array(ADMIN, $roleList) || in_array(STUDENT, $roleList) || in_array(TEACHER, $roleList)) {
+			$navbarItem .= '<li><a href="/sessions"><i class="ri-slideshow-3-line"></i> ' . lang("NAVBAR_SESSION") . '</a></li>';
+		}
+
+		// Admin pages
+		if (in_array(ADMIN, $roleList)) {
+			$navbarItem .= '<li><a href="/utilisateurs"><i class="ri-folder-line"></i> ' . lang("NAVBAR_USER") . '</a></li>';
+			$navbarItem .= '<li><a href="/logs?trace"><i class="ri-computer-line"></i> ' . lang("NAVBAR_LOG") . '</a></li>';
+		}
+
+		// Profil pages
+		$navbarItem .= '<li><div class="profil"><a class="not-profil"><i class="ri-user-line"></i>' . nameFormat($_SESSION["login"], true) . '</a>';
+		$navbarItem .= '<ul><div class="sub"><li><a href="/compte"><i class="ri-account-circle-line"></i>' . lang("NAVBAR_PROFIL_ACCOUNT") . '</a></li>';
+		$navbarItem .= '<li><a href="/deconnexion"><i class="ri-logout-box-line"></i>' . lang("NAVBAR_PROFIL_DISCONNECT") . '</a></li></div></ul></div></li>';
+
+		// Theme
+		$navbarItem .= '<li><form method="POST"><input type="hidden" name="lang" value="' . $userSetting["lang"] . '">';
+		$navbarItem .= '<button class="buttonTheme" type="submit" name="theme" value="' . $theme . '">' . $icon . '</button></form></li>';
+	}
+	echo $navbarItem;
+	?>
+</nav>
+
+<!-- Background -->
+<style>
+	body {
 		<?php
-		// Check if user is connected
-		if (!isset($_SESSION["login"])) { ?>
-		<li>
-			<a href="/login"><i class="ri-user-line"></i> <?= lang("NAVBAR_CONNECT") ?></a>
-		</li>
-	</nav>
-		<?php
-		// If the user is connected
-		} else {
-			$navbarItem = "";
-			$roleList = permissionChecker(true, "");
+		// Check if user is connected to change his theme
+		if (isset($_SESSION["login"])) {
 			$userSetting = UserRepository::getSetting($_SESSION["login"]);
 
-			if (in_array(TEACHER, $roleList)) {
-				$navbarItem .= '<li><a href="/classes"><i class="ri-folder-line"></i> ' . lang("NAVBAR_CLASS") . '</a></li>';
-				$navbarItem .= '<li><a href="/sessioncreation"><i class="ri-computer-line"></i> ' . lang("NAVBAR_CREATE_SESSION") . '</a></li>';
-			}
+			if ($userSetting["theme"] == "0")
+				$theme = "light";
+			else
+				$theme = "dark";
 
-			if (in_array(ADMIN, $roleList) || in_array(STUDENT, $roleList) || in_array(TEACHER, $roleList)) {
-				$navbarItem .= '<li><a href="/sessions"><i class="ri-slideshow-3-line"></i> ' . lang("NAVBAR_SESSION") . '</a></li>';
-			}
-
-			if (in_array(ADMIN, $roleList)) {
-				$navbarItem .= '<li><a href="/utilisateurs"><i class="ri-folder-line"></i> ' . lang("NAVBAR_USER") . '</a></li>';
-				$navbarItem .= '<li><a href="/logs?trace"><i class="ri-computer-line"></i> ' . lang("NAVBAR_LOG") . '</a></li>';
-			}
-
-			echo $navbarItem;
-
+			echo 'background-image: url("/public/img/background/' . $theme .'/default.png")';
+		} else {
+			echo 'background-image: url("/public/img/background/light/default.png")';
+		}
 		?>
-		<li>
-			<div class="profil">
-			<a class="not-profil"><i class="ri-user-line"></i> <?= nameFormat($_SESSION["login"], true) ?></a>
-			<ul>
-				<div class="sub">
-				<li>
-					<a href="/compte"><i class="ri-account-circle-line"></i> <?= lang("NAVBAR_PROFIL_ACCOUNT") ?></a>
-				</li>
-				<li>
-					<a href="/deconnexion"><i class="ri-logout-box-line"></i> <?= lang("NAVBAR_PROFIL_DISCONNECT") ?></a>
-				</li>
-				</div>
-			</ul>
-		</li>
-		<li>
-			<form method="POST">
-				<?php
-				if ($userSetting["theme"] == "0"){
-					$theme = "dark";
-					$icon = "<i class='ri-moon-line'></i>";
-				}
-				else {
-					$theme = "light";
-					$icon = "<i class='ri-sun-line'></i>";
-				}
-				?>
-				<input type="hidden" name="lang" value="<?= $userSetting["lang"] ?>">
-				<button class="buttonTheme" type="submit" name="theme" value="<?= $theme ?>"><?= $icon ?></button>
-			</form>
-		</li>
-		</div>
-	</nav>
-	<?php
-		}
-	?>
-
-	<!-- Background -->
-	<style>
-		body {
-			<?php
-			// Check if user is connected to change his theme
-			if (isset($_SESSION["login"])) {
-				$userSetting = UserRepository::getSetting($_SESSION["login"]);
-
-				if ($userSetting["theme"] == "0")
-					$theme = "light";
-				else
-					$theme = "dark";
-				?>
-				background-image: url("/public/img/background/<?=$theme?>/default.png");
-			<?php
-			} else { ?>
-				background-image: url("/public/img/background/light/default.png");
-			<?php
-			} ?>
-		}
-	</style>
-
-	<div class="main">
+	}
+</style>
