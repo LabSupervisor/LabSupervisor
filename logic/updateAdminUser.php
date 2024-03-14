703fd3ca7ecc463a70b39a63ea11bf25913463cf
marine404 <marine.gonnord.pro@gmail.com>
2024-03-14 13:03:26 +0100

    modif classroom user in progress

--- logic/updateAdminUser.php
+++ logic/updateAdminUser.php
@@ -1,5 +1,18 @@
 <?php
+
+if (isset($_POST['selectedClassroom'])) {
+	// Récupère la valeur sélectionnée du menu déroulant
+	$selectedClassroom = $_POST['selectedClassroom'];
+
+	// Utilise la valeur sélectionnée comme bon te semble
+	echo "La classe sélectionnée est : " . $selectedClassroom;
+} else {
+	echo "Aucune classe sélectionnée.";
+}
+
+
 if (isset($_POST["modify"])) {
+	echo "";
 	$userRepo = new UserRepository();
 	$email = UserRepository::getEmail($_POST["userId"]);
 
--- page/user.php
+++ page/user.php
@@ -14,6 +14,15 @@
 
 <link rel="stylesheet" href="/public/css/user.css">
 
+<?php
+$tabClass = ClassroomRepository::getClassrooms();
+// Convertit le tableau PHP en chaîne JSON pour l'utiliser en JavaScript
+$classes_json = json_encode($tabClass);
+
+
+?>
+
+
 <script>
 	// Create update fields
 	function updateUser(userId) {
@@ -21,8 +30,10 @@
 
 		var surnameElement = document.getElementById("surname_" + userId);
 		var nameElement = document.getElementById("name_" + userId);
+		var classRoomElement = document.getElementById("classroom_" + userId);
 		var surname = surnameElement.innerHTML;
 		var name = nameElement.innerHTML;
+		var classRoom = surnameElement.innerHTML ;
 
 		var modifyButtonDisable = document.getElementsByClassName("modifybutton");
 
@@ -48,8 +59,35 @@
 		inputName.setAttribute("placeholder", "<?= lang("USER_UPDATE_NAME") ?>");
 		inputName.setAttribute("value", name);
 
+		var selectClassroom = document.createElement("select");
+		selectClassroom.setAttribute("id", "selectedClassroom");
+		selectClassroom.setAttribute("name", "selectedClassroom");
+		// Récupère les classes depuis la chaîne JSON
+		var classrooms = <?php echo $classes_json; ?>;
+		// Ajoute les options au menu déroulant
+		for (var i = 0; i < classrooms.length; i++) {
+			var option = document.createElement("option");
+			option.text = classrooms[i].name; // Utilise la propriété "name" de chaque objet
+			selectClassroom.add(option);
+		}
+
+		// Ajoute un gestionnaire d'événements pour détecter le changement de sélection dans le menu déroulant
+		selectClassroom.addEventListener("change", function() {
+
+			// Récupère l'option sélectionnée dans le menu déroulant
+			var selectedOption = selectClassroom.options[selectClassroom.selectedIndex];
+
+			// Récupère la valeur de l'option sélectionnée (le nom de la classe)
+			var selectedValue = selectedOption.value;
+
+			// Met à jour la valeur de l'élément input caché pour cet utilisateur
+			var hiddenInputId = "selectedClassroom_" + userId;
+			document.getElementById(hiddenInputId).value = selectedValue;
+		});
+
 		surnameElement.replaceChildren(inputSurname);
 		nameElement.replaceChildren(inputName);
+		classRoomElement.replaceChildren(selectClassroom);
 
 		var inputUserId = document.createElement("input");
 		inputUserId.setAttribute("type", "hidden");
@@ -93,19 +131,21 @@
 				<td class="col2" id="name_<?=$userId?>"><?=$user['name']?></td>
 				<td class="col3"><?=$user['email']?></td>
 				<td class="col4"> <?=roleFormat($user['email'])?></td>
-				<?php
+				<td class="col5" id="classroom_<?=$userId?>">
+					<?php
 					if ($user["classroom"]) {
-						echo "<td class='col5'>" . $user["classroom"] . "</td>";
+						echo $user["classroom"];
 					} else {
-						echo "<td class='col5'>" . lang("USER_UPDATE_CLASS_EMPTY") . "</td>";
+						echo lang("USER_UPDATE_CLASS_EMPTY");
 					}
-				?>
+					?>
+					<input type="hidden" name="selectedClassroom" id="selectedClassroom_<?=$userId?>">
+				</td>
 				<td class="col6"><button class="modifybutton button" type="button" id="modify_<?=$userId?>" onclick="updateUser(<?=$userId?>)"><?= lang("USER_UPDATE_MODIFY") ?></button>
 				<form method="POST" action="#">
 					<input type="hidden" name="userId" value="<?= $userId ?>">
 					<button class="button" type="submit" name="send" id="delete_<?= $userId ?>"><?= lang("USER_UPDATE_DELETE") ?></button>
 				</form>
-
 				</td>
 			</tr>
 			<?php
