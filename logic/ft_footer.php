d476f91fd8ef379efdea56c385a078abf7d24c20
marine404 <marine.gonnord.pro@gmail.com>
2024-03-15 12:07:06 +0100

    start footer

---
+++ logic/ft_footer.php
@@ -0,0 +1,32 @@
+<?php
+	// Logic
+	require($_SERVER["DOCUMENT_ROOT"] . "/logic/ft_nameFormat.php");
+	require($_SERVER["DOCUMENT_ROOT"] . "/logic/updateTheme.php");
+
+	$navbarItem = "";
+?>
+
+<body>
+
+<footer class="footer">
+
+<table>
+	<thead>
+		<tr >
+			<th> *Logo* labsup</th>
+			<th> Companie </th>
+			<th> Ressources </th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>© crédit </td>
+			<td>pepouz </td>
+			<td>Ta mere </td>
+		</tr>
+	</tbody>
+</table>
+<div> Créer dans le cadre d'un projet scolaire </div>
+
+</footer>
+
