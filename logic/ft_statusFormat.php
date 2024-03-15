2ae03470d7da79214b71a0a38d1b90febf455c6e
SkyWors <skyworslegrand@gmail.com>
2024-03-15 22:29:57 +0100

    Add status precisions

--- logic/ft_statusFormat.php
+++ logic/ft_statusFormat.php
@@ -1,14 +1,23 @@
 <?php
 
 function statusFormat($statusId) {
-	if ($statusId == 0)
+	$status = "";
+	$text = "";
+	if ($statusId == 0) {
 		$status = "";
-	if ($statusId == 1)
+	}
+	if ($statusId == 1) {
 		$status = "statusRed";
-	if ($statusId == 2)
+		$text = lang("DASHBOARD_STATUS_RED");
+	}
+	if ($statusId == 2) {
 		$status = "statusYellow";
-	if ($statusId == 3)
+		$text = lang("DASHBOARD_STATUS_YELLOW");
+	}
+	if ($statusId == 3) {
 		$status = "statusGreen";
+		$text = lang("DASHBOARD_STATUS_GREEN");
+	}
 
-	return "<div class='statusBall $status'></div>";
+	return "<div class='statusBall $status'><a>$text</a></div>";
 }
