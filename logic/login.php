f62424ea28e63e78a3b0a3533bc9900b948d049f
marine404 <marine.gonnord.pro@gmail.com>
2024-03-18 15:06:36 +0100

    error display in progress

--- logic/login.php
+++ logic/login.php
@@ -9,5 +9,19 @@ if (isset($_POST["login"])) {
 		}
 	}
 	// Default error
-	echo lang("LOGIN_ERROR_NOTFOUND");
+	echo '<script>
+	var popup = document.createElement("div");
+	popup.classList.add("custom-popup");
+	popup.textContent = " ' . lang("LOGIN_ERROR_NOTFOUND") . ' ";
+
+	var closeButton = document.createElement("button");
+	closeButton.classList.add("button");
+	closeButton.textContent = "OK";
+	closeButton.addEventListener("click", function() {
+		popup.remove();
+	});
+
+	popup.appendChild(closeButton);
+	document.body.appendChild(popup);
+	</script>';
 }
