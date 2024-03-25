6a0d8cce7c1e3e2ce1271f5d8c69a30dffa6fd81
SkyWors <skyworslegrand@gmail.com>
2024-03-25 21:12:00 +0100

    Done implement new icon

--- logic/ft_uidGen.php
+++ logic/ft_uidGen.php
@@ -2,13 +2,13 @@
 
 function uidGen() {
 	$char = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_";
-    $result = "";
-    $randomByte = random_bytes(10);
+	$result = "";
+	$randomByte = random_bytes(10);
 
-    foreach (str_split($randomByte) as $byte) {
-        $random = ord($byte) % strlen($char);
-        $result .= $char[$random];
-    }
+	foreach (str_split($randomByte) as $byte) {
+		$random = ord($byte) % strlen($char);
+		$result .= $char[$random];
+	}
 
-    return $result;
+	return $result;
 }
