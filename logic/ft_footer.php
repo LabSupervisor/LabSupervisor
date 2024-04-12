d8990be72c4bdf3badc226d165df87fc63b6440d
SkyWors <skyworslegrand@gmail.com>
2024-04-12 22:26:24 +0200

    Update composer psr4

--- logic/ft_footer.php
+++
@@ -1,6 +0,0 @@
-<!-- close main -->
-</div>
-
-<footer class="footer">
-	<a><?= lang("MAIN_TITLE") ?> © ORT LYON 2024</a>
-</footer>
--- test/role.php
+++ test/role.php
@@ -3,5 +3,5 @@
 require($_SERVER["DOCUMENT_ROOT"] . '/function/ft_header.php');
 mainHeader("Role Test");
 
-permissionChecker(true, true, false, true);
+permissionChecker(true, array(ADMIN, STUDENT, TEACHER));
 echo "Access!";
