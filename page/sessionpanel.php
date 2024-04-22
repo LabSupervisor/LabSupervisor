76c26ee8fa5b74d99c01d390571118d24291d9a4
SkyWors <skyworslegrand@gmail.com>
2024-04-22 16:30:43 +0200

    Update session panel lang

--- page/sessionpanel.php
+++ page/sessionpanel.php
@@ -42,9 +42,9 @@
 	<table>
 		<thead>
 			<tr>
-				<th>Chapitre</th>
-				<th>Action</th>
-				<th>Status</th>
+				<th><?= lang("SESSION_PANEL_CHAPTER") ?></th>
+				<th><?= lang("SESSION_PANEL_ACTION") ?></th>
+				<th><?= lang("SESSION_PANEL_STATUS") ?></th>
 			</tr>
 		</thead>
 		<tbody>
@@ -68,9 +68,9 @@
 					<td class="col1"><?= $chapter["title"] ?></td>
 					<td class="col2">
 						<input type="hidden" name="liste" value="<?php echo $chapter['id']; ?>">
-						<button class="button" onclick="setStatus(<?= $chapter['id'] ?>, 1)"><i class="ri-error-warning-line"></i>J'ai besoin d'aide !</button>
-						<button class="button" onclick="setStatus(<?= $chapter['id'] ?>, 2)"><i class="ri-edit-line"></i> Travail en cours...</button>
-						<button class="button" onclick="setStatus(<?= $chapter['id'] ?>, 3)"><i class="ri-thumb-up-line"></i> Terminé !</button>
+						<button class="button" onclick="setStatus(<?= $chapter['id'] ?>, 1)"><i class="ri-error-warning-line"></i> <?= lang("SESSION_PANEL_HELP") ?></button>
+						<button class="button" onclick="setStatus(<?= $chapter['id'] ?>, 2)"><i class="ri-edit-line"></i> <?= lang("SESSION_PANEL_WIP") ?></button>
+						<button class="button" onclick="setStatus(<?= $chapter['id'] ?>, 3)"><i class="ri-thumb-up-line"></i> <?= lang("SESSION_PANEL_DONE") ?></button>
 					</td>
 					<td class="col3"><div class="<?= $statusBall ?>" id="statusBall_<?= $chapter['id'] ?>"></div></td>
 				</tr>
@@ -83,20 +83,20 @@
 
 <div class="item">
 	<div class="mainbox screenshareBox">
-		<h2>Partage d'écran</h2>
-		<button class="button" id="shareButton">Start screenshare</button>
+		<h2><?= lang("SESSION_PANEL_SCREENSHARE") ?></h2>
+		<button class="button" id="shareButton"><i class="ri-share-line"></i> <?= lang("SESSION_PANEL_SCREENSHARE_START") ?></button>
 	</div>
 
 	<div class="mainbox lslinkBox">
-		<h2>LS-Link</h2>
+		<h2><?= lang("SESSION_PANEL_LSLINK") ?></h2>
 
 		<?php
-			$buttonText = "Connecter";
+			$buttonText = '<i class="ri-link"></i> ' . lang("SESSION_PANEL_LSLINK_CONNECT");
 			if (UserRepository::getLink($_SESSION["login"], $_SESSION["session"])){
-				echo "Actuellement connecté à LS-LINK #" . UserRepository::getLink($_SESSION["login"], $_SESSION["session"]);
-				$buttonText = "Modifier";
+				echo lang("SESSION_PANEL_LSLINK_NUMBER") . UserRepository::getLink($_SESSION["login"], $_SESSION["session"]);
+				$buttonText = '<i class="ri-pencil-line"></i> ' . lang("SESSION_PANEL_LSLINK_MODIFY");
 			}
-			echo "<form method='POST'><input type='hidden' name='sessionId' value='" . $_SESSION["session"] . "'><input class='lslinkid' type='number' name='number'/><button class='button' type='submit' name='link'><i class=\"ri-link\"></i> " . $buttonText . "</button></form>";
+			echo "<form method='POST'><input type='hidden' name='sessionId' value='" . $_SESSION["session"] . "'><input class='lslinkid' type='number' name='number'/><button class='button' type='submit' name='link'>" . $buttonText . "</button></form>";
 		?>
 	</div>
 </div>
