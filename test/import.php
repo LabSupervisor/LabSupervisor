0f98b27cf415a365fbe88a6f7141aa53fac19a20
SkyWors <skyworslegrand@gmail.com>
2024-04-22 12:11:27 +0200

    Start adding unit test

---
+++ test/import.php
@@ -0,0 +1,25 @@
+<?php
+
+use
+	LabSupervisor\app\repository\DatabaseRepository,
+	LabSupervisor\app\repository\ActiveDirectoryRepository;
+
+// Load .env file
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__FILE__) . "/../")->load();
+
+// Create constants
+define("DEFAULT_LANGUAGE", $_ENV['DEFAULT_LANGUAGE']);
+define("DEFAULT_THEME", $_ENV['DEFAULT_THEME']);
+
+define("ADMIN", 1);
+define("STUDENT", 2);
+define("TEACHER", 3);
+
+// Declare database connection
+$db = new DatabaseRepository;
+define("DATABASE", $db->getConnection());
+
+if ($_ENV["AUTHENTIFICATION_TYPE"] == "ad") {
+	$ad = new ActiveDirectoryRepository;
+	define("AD", $ad->getConnection());
+}
