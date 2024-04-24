23e9fcbb28af798e8992b338ce3ffb11664e4b4c
SkyWors <skyworslegrand@gmail.com>
2024-04-24 12:22:53 +0200

    Refactor classroom page

--- page/classroom.php
+++ page/classroom.php
@@ -1,4 +1,5 @@
 <?php
+
 	use
 		LabSupervisor\app\repository\ClassroomRepository,
 		LabSupervisor\app\repository\UserRepository;
@@ -23,20 +24,20 @@
 	$classrooms = ClassroomRepository::getClassrooms();
 
 	// Side menu
-	echo '<div id="lateral-selector" class="mainbox">';
+	echo '<div class="mainGroup"><div id="lateralSelector" class="mainbox">';
 	echo "<h2>" . lang("CLASSROOM_NAME") . "</h2>";
 
 	for($i = 0; $i < count($classrooms); $i++) {
 		$classroom = $classrooms[$i];
 
 		$selected = "";
-		if($i == 0) // Focus first class
+		// Focus first class
+		if($i == 0)
 			$selected = "selected";
 
 		if ($classroom["active"] == "1") {
-			echo '<div id="class-' . $classroom["id"] . '" class="classname" ' . $selected . ' onclick="selectClass(' . $classroom["id"] . ')">' . $classroom["name"] . '</div>';
+			echo '<div id="classroom_' . $classroom["id"] . '" class="classname" ' . $selected . ' onclick="selectClass(' . $classroom["id"] . ')">' . $classroom["name"] . '</div>';
 		}
-
 	}
 	echo '</div>';
 
@@ -51,7 +52,7 @@
 				$selected = "selected";
 ?>
 
-			<div id="content-class-<?php echo $classroom["id"]?>" class="mainbox content-class" <?php echo $selected?>>
+			<div id="contentClassroom_<?php echo $classroom["id"]?>" class="mainbox contentClassroom" <?php echo $selected?>>
 			<h2><?php echo $classroom["name"]?></h2>
 			<table>
 				<thead>
@@ -80,8 +81,8 @@
 							<td>
 								<!-- Delete user form -->
 								<form action="" method="post">
-									<input type="hidden" name="class_id" value="<?= $classroom["id"] ?>">
-									<input type="hidden" name="remove_student" value="<?= $student["iduser"] ?>">
+									<input type="hidden" name="classroomId" value="<?= $classroom["id"] ?>">
+									<input type="hidden" name="removeStudent" value="<?= $student["iduser"] ?>">
 									<button type="submit" class="button"><i class="ri-eraser-line"></i> <?= lang("CLASSROOM_STUDENT_REMOVE") ?></button>
 								</form>
 							</td>
@@ -98,16 +99,16 @@
 			// Free student list
 			$freeStudents = ClassroomRepository::getUsersNotInClassroom();
 			if ($freeStudents) {
-				echo "<form class='free-students' method='POST'>";
-				echo "<input type='hidden' name='class_id' value=" . $classroom["id"] .">";
-				echo "<select class='select-student' name='student_id' id='student_id'><label for='student_id'>Sélectionner un élève :</label>";
+				echo "<form class='freeStudents' method='POST'>";
+				echo "<input type='hidden' name='classroomId' value=" . $classroom["id"] .">";
+				echo "<select class='selectStudent' name='studentId' id='studentId'>";
 				foreach ($freeStudents as $student)	{
-					echo "<option value='" . $student['id'] . "'>" . $student['name'] . " " . $student['surname'] . "</option>";
+					echo "<option value='" . $student['id'] . "'>" . $student['surname'] . " " . $student['name'] . "</option>";
 				}
-				echo "</select><input type='submit' class='button' name='add_student' value='Ajouter'></form>";
+				echo "</select><button type='submit' class='button' name='addStudent'><i class=\"ri-add-line\"></i> " . lang("CLASSROOM_ADD") . "</button></form>";
 			}
 		?>
-			</div>
+			</div></div>
 		<?php
 		}
 	}
