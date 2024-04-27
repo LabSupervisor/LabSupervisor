5372a0a8e22f271410e962363660e866df293073
SkyWors <skyworslegrand@gmail.com>
2024-04-27 23:21:08 +0200

    Add main page box translation

--- page/index.php
+++ page/index.php
@@ -19,19 +19,19 @@
 		<div class="boxGroup">
 			<div class="mainbox">
 				<h2><?= lang("NAVBAR_SESSION") ?></h2>
-				<a>Sur cette page vous pouvez consulter toutes les sessions en cours créées par les professeurs, en tant qu'administrateurs vous pouvez seulements les consulter</a>
+				<a><?= lang("INDEX_ADMIN_SESSION") ?></a>
 				<button class="button" onclick="window.location.href = '/sessions'"><i class="ri-slideshow-3-line"></i> <?= lang("NAVBAR_SESSION") ?></button>
 			</div>
 
 			<div class="mainbox">
 				<h2><?= lang("NAVBAR_USER") ?></h2>
-				<a>Vous pouvez superviser tous les utilisateurs du site sur cette page; c'est a dire les modifiers a votre guise et les supprimer de la base de données du site web</a>
+				<a><?= lang("INDEX_ADMIN_USER") ?></a>
 				<button class="button" onclick="window.location.href = '/utilisateurs'"><i class="ri-folder-line"></i> <?= lang("NAVBAR_USER") ?></button>
 			</div>
 
 			<div class="mainbox">
 				<h2><?= lang("NAVBAR_LOG") ?></h2>
-				<a>Ici vous pouvez consulter l'historique des modifications des autres admins</a>
+				<a><?= lang("INDEX_ADMIN_LOG") ?></a>
 				<button class="button" onclick="window.location.href = '/logs?trace'"><i class="ri-computer-line"></i> <?= lang("NAVBAR_LOG") ?></button>
 			</div>
 		</div>
@@ -42,7 +42,7 @@
 		<div class="boxGroup">
 			<div class="mainbox">
 				<h2><?= lang("NAVBAR_SESSION") ?></h2>
-				<a>D'ici vous pouvez consulter toutes les sessions auquels vous avez été inscrit</a>
+				<a><?= lang("INDEX_STUDENT_SESSION") ?></a>
 				<button class="button" onclick="window.location.href = '/sessions'"><i class="ri-slideshow-3-line"></i> <?= lang("NAVBAR_SESSION") ?></button>
 			</div>
 		</div>
@@ -53,17 +53,17 @@
 		<div class="boxGroup">
 			<div class="mainbox">
 				<h2><?= lang("NAVBAR_CLASS") ?></h2>
-				<a>Sur cette page vous pouvez consulter les classes auquels vous avez été attitré</a>
+				<a><?= lang("INDEX_TEACHER_CLASSROOM") ?></a>
 				<button class="button" onclick="window.location.href = '/classes'"><i class="ri-folder-line"></i> <?= lang("NAVBAR_CLASS") ?></button>
 			</div>
 			<div class="mainbox">
 				<h2><?= lang("NAVBAR_CREATE_SESSION") ?></h2>
-				<a>C'est avec cette page que vous pouvez crée une session ,une fois la session crée vous pourrez la consulté grace au boutons Mes sessions ci-dessous</a>
+				<a><?= lang("INDEX_TEACHER_CREATE_SESSION") ?></a>
 				<button class="button" onclick="window.location.href = '/sessioncreation'"><i class="ri-computer-line"></i> <?= lang("NAVBAR_CREATE_SESSION") ?></button>
 			</div>
 			<div class="mainbox">
 				<h2><?= lang("NAVBAR_SESSION") ?></h2>
-				<a>Ici vous pouvez consulter les sessions auquels vous avez été inscrit ou que vous avez vous meme crée</a>
+				<a><?= lang("INDEX_TEACHER_SESSION") ?></a>
 				<button class="button" onclick="window.location.href = '/sessions'"><i class="ri-slideshow-3-line"></i> <?= lang("NAVBAR_SESSION") ?></button>
 			</div>
 		</div>
