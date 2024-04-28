ce1af8088890f9365cdb42eaa6f490122e08d184
SkyWors <skyworslegrand@gmail.com>
2024-04-28 23:12:32 +0200

    Add page system on admin log page

--- page/log.php
+++ page/log.php
@@ -20,6 +20,12 @@
 <?php
 	// If traces are ask
 	if (isset($_GET["trace"])) {
+		$logs = array();
+		$i = 0;
+		foreach (LogRepository::getLogs() as $log) {
+			$logs[$i] = $log;
+			$i++;
+		}
 ?>
 
 <div class="mainbox buttonContainer">
@@ -39,22 +45,44 @@
 		</thead>
 		<tbody>
 			<?php
-			$logs = LogRepository::getLogs();
+			if (!isset($_GET["page"])) {
+				$_GET["page"] = 1;
+			}
 
+			$i = 0;
+			$max = 20;
 			foreach ($logs as $line) {
-				$userInfo = UserRepository::getInfo($line["iduser"]);
-				$username = $userInfo["name"] . " " . $userInfo["surname"];
+				if ($i >= ($_GET["page"] -1) * $max && $i < $_GET["page"] * $max) {
+					$userInfo = UserRepository::getInfo($line["iduser"]);
+					$username = $userInfo["name"] . " " . $userInfo["surname"];
 
-				echo "<tr>";
-				echo '<td>' . $line["id"] . '</td>';
-				echo '<td>' . $username . '</td>';
-				echo '<td class="col3">' . $line["message"] . '</td>';
-				echo '<td class="col4">' . $line["date"] . '</td>';
-				echo "</tr>";
+					echo "<tr>";
+					echo '<td>' . $line["id"] . '</td>';
+					echo '<td>' . $username . '</td>';
+					echo '<td class="col3">' . $line["message"] . '</td>';
+					echo '<td class="col4">' . $line["date"] . '</td>';
+					echo "</tr>";
+				}
+				$i++;
 			}
 			?>
 		</tbody>
 	</table>
+	<form class="pageGroup" method="GET">
+		<input type="hidden" name="trace">
+		<?php
+			if ($_GET["page"] != 1) {
+		?>
+		<button class="button" type="submit" name="page" value="<?= $_GET["page"] -1 ?>"><i class="ri-arrow-left-s-line"></i></button>
+		<?php
+			}
+			if (count($logs) >= $_GET["page"] * $max) {
+		?>
+		<button class="button" type="submit" name="page" value="<?= $_GET["page"] +1 ?>"><i class="ri-arrow-right-s-line"></i></button>
+		<?php
+			}
+		?>
+	</form>
 </div>
 <?php
 // If errors are ask
@@ -106,36 +134,59 @@
 
 			$log = preg_split("/\[" . $fileDate . " (.*?)\]/", $logs);
 
+			if (!isset($_GET["page"])) {
+				$_GET["page"] = 1;
+			}
+
+			$max = 20;
 			$i = -1;
 			foreach($log as $temp) {
-				// Get error's values
-				$message = "";
-				$error = "";
+				if ($i >= ($_GET["page"] -1) * $max && $i < $_GET["page"] * $max) {
+					// Get error's values
+					$message = "";
+					$error = "";
 
-				$data = explode("\n", $temp);
-				foreach($data as $value) {
-					if (!str_starts_with($value, "#")) {
-						if (strlen($value) > 1) {
-							$error = $value;
+					$data = explode("\n", $temp);
+					foreach($data as $value) {
+						if (!str_starts_with($value, "#")) {
+							if (strlen($value) > 1) {
+								$error = $value;
+							}
+						} else {
+							$message .= $value . " ";
 						}
-					} else {
-						$message .= $value . " ";
 					}
-				}
 
-				// Prevent showing empty line
-				if (strlen($error) > 1) {
-					echo "<tr>";
-					echo "<td class='col3'><div class='col3' title='" . htmlspecialchars($error) . "'>" . htmlspecialchars($error) . "</div></td>";
-					echo "<td class='col3'><div class='col3' title='" . str_replace("##", "#", htmlspecialchars($message)) . "'>" . htmlspecialchars(str_replace("##", "#", $message)) . "</div></td>";
-					echo "<td class='col3'>" . $listDate[$i] . "</td>";
-					echo "</tr>";
+					// Prevent showing empty line
+					if (strlen($error) > 1) {
+						echo "<tr>";
+						echo "<td class='col3'><div class='col3' title='" . htmlspecialchars($error) . "'>" . htmlspecialchars($error) . "</div></td>";
+						echo "<td class='col3'><div class='col3' title='" . str_replace("##", "#", htmlspecialchars($message)) . "'>" . htmlspecialchars(str_replace("##", "#", $message)) . "</div></td>";
+						echo "<td class='col3'>" . $listDate[$i] . "</td>";
+						echo "</tr>";
+					}
 				}
 				$i++;
 			}
 		?>
 		</tbody>
 	</table>
+	<form class="pageGroup" method="GET">
+		<input type="hidden" name="error">
+		<input type="hidden" name="date" value="<?= $_GET['date']?>">
+		<?php
+			if ($_GET["page"] != 1) {
+		?>
+		<button class="button" type="submit" name="page" value="<?= $_GET["page"] -1 ?>"><i class="ri-arrow-left-s-line"></i></button>
+		<?php
+			}
+			if (count($log) >= $_GET["page"] * $max) {
+		?>
+		<button class="button" type="submit" name="page" value="<?= $_GET["page"] +1 ?>"><i class="ri-arrow-right-s-line"></i></button>
+		<?php
+			}
+		?>
+	</form>
 </div>
 
 	<?php
