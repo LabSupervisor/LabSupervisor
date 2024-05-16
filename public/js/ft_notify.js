e08ef62bf2a64ab9f9c3cdbe1a6846739ccede63
SkyWors <skyworslegrand@gmail.com>
2024-05-16 18:50:13 +0200

    Update notifications style

--- public/js/ft_notify.js
+++ public/js/ft_notify.js
@@ -3,7 +3,12 @@ if (Notification && Notification.permission !== "denied") {
 }
 
 function notify(msg) {
-	if (Notification?.permission === "granted") {
-		new Notification(msg);
+	if (Notification.permission === "granted") {
+		new Notification(lang("MAIN_TITLE"), {
+				body: msg,
+				icon: "/public/img/icon/logo.png",
+				tag: "soManyNotification"
+			}
+		);
 	}
 }
