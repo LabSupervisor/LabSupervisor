dcfd51a5af0b50850f91cb710b3c737268318d98
SkyWors <skyworslegrand@gmail.com>
2024-05-27 11:54:10 +0200

    Keep register form value when password error

--- page/register.php
+++ page/register.php
@@ -9,6 +9,15 @@
 	// Logic
 	echo '<script src="/public/js/ft_popup.js"></script>';
 	require($_SERVER["DOCUMENT_ROOT"] . '/logic/register.php');
+
+	$identifiant = "";
+	$name = "";
+	$surname = "";
+	if (isset($_POST["email"])) {
+		$identifiant = $_POST["email"];
+		$name = $_POST["name"];
+		$surname = $_POST["surname"];
+	}
 ?>
 
 <link rel="stylesheet" href="/public/css/form.css">
@@ -18,7 +27,7 @@
 		<h2><i class="ri-user-line"></i> <?= lang("REGISTER_TITLE") ?></h2>
 	</div>
 	<div>
-		<input type="email" name="email" placeholder="<?= lang("REGISTER_EMAIL") ?>" required autofocus>
+		<input type="email" name="email" placeholder="<?= lang("REGISTER_EMAIL") ?>" value="<?= $identifiant ?>" required autofocus>
 	</div>
 	<div>
 		<input type="password" id="password" name="password" placeholder="<?= lang("REGISTER_PASSWORD") ?>" required>
@@ -29,10 +38,10 @@
 		<button class="showPasswordButton" tabindex="-1" type="button" id="showPasswordButton" onclick="togglePasswordVisibility('passwordConf', 'eyeIconConf')"><i id="eyeIconConf" class="ri-eye-off-line"></i></button>
 	</div>
 	<div>
-		<input type="text" name="name" placeholder="<?= lang("MAIN_NAME") ?>" class="Name" required>
+		<input type="text" name="name" placeholder="<?= lang("MAIN_NAME") ?>" class="Name" value="<?= $name ?>" required>
 	</div>
 	<div>
-		<input type="text" name="surname" placeholder="<?= lang("MAIN_SURNAME") ?>" required>
+		<input type="text" name="surname" placeholder="<?= lang("MAIN_SURNAME") ?>" value="<?= $surname ?>" required>
 	</div>
 	<div>
 		<button class="button" type="submit" name="register"><i class="ri-id-card-line"></i> <?= lang("REGISTER_SUBMIT") ?></button>
