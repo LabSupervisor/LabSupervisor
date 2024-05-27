8d225c970ad0f00a846d69fcec60925bf57668ea
SkyWors <skyworslegrand@gmail.com>
2024-05-27 10:38:52 +0200

    Fixed header issue on student panel

--- logic/createLink.php
+++ logic/createLink.php
@@ -5,11 +5,9 @@ use LabSupervisor\app\repository\UserRepository;
 if (isset($_POST["link"])) {
 	// Connect LS-Link with user
 	UserRepository::link($_SESSION["login"], $_POST["sessionId"], $_POST["number"]);
-	header("Location: /panel");
 }
 
 if (isset($_POST["disconnect"])) {
 	// Disconnect LS-Link with user
 	UserRepository::unlink($_SESSION["login"], $_SESSION["session"], $_POST["disconnect"]);
-	header("Location: /panel");
 }
