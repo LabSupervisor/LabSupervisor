e86fb6b7ab234ed488f7eda78f3b1b21f8c899d5
SkyWors <skyworslegrand@gmail.com>
2024-05-28 14:48:42 +0200

    OMG SECURITY PATCH WAAA, dumb security issue fixed.

--- src/app/repository/UserRepository.php
+++ src/app/repository/UserRepository.php
@@ -20,7 +20,6 @@ class UserRepository {
 
 				// Hash password using bcrypt
 				$password = password_hash($bindParam["password"], PASSWORD_BCRYPT);
-				LogRepository::fileSave(new Exception($bindParam["password"]));
 
 				// Create user
 				try {
