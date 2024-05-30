2b07af2365f1611504224e48de2ea0d612908e1c
SkyWors <skyworslegrand@gmail.com>
2024-05-31 00:23:24 +0200

    Update database

--- sql/database.sql
+++ sql/database.sql
@@ -130,6 +130,17 @@ CREATE TABLE `userclassroom` (
   KEY `usergroup_group_FK` (`idclassroom`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+DROP TABLE IF EXISTS `teacherclassroom`;
+CREATE TABLE `teacherclassroom` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `iduser` int(11) NOT NULL,
+  `idclassroom` int(11) NOT NULL,
+  `updatedate` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `teacherclassroom_user_FK` (`iduser`),
+  KEY `teacherclassroom_classroom_FK` (`idclassroom`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
 DROP TABLE IF EXISTS `link`;
 CREATE TABLE `link` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -171,6 +182,10 @@ ALTER TABLE `log`
 ALTER TABLE `userclassroom`
   ADD CONSTRAINT `usergroup_group_FK` FOREIGN KEY (`idclassroom`) REFERENCES `classroom` (`id`),
   ADD CONSTRAINT `usergroup_user_FK` FOREIGN KEY (`iduser`) REFERENCES `user` (`id`);
+
+ALTER TABLE `teacherclassroom`
+  ADD CONSTRAINT `teacherclassroom_classroom_FK` FOREIGN KEY (`idclassroom`) REFERENCES `classroom` (`id`),
+  ADD CONSTRAINT `teacherclassroom_user_FK` FOREIGN KEY (`iduser`) REFERENCES `user` (`id`);
 
 ALTER TABLE `link`
   ADD CONSTRAINT `link_user_FK` FOREIGN KEY (`iduser`) REFERENCES `user` (`id`),
