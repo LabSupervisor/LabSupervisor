d7b483dfc6897e29a3a01c673fda2d05e8525854
SkyWors <skyworslegrand@gmail.com>
2024-05-31 23:23:26 +0200

    Fixed index box file

--- public/js/code.js
+++ public/js/code.js
@@ -1,6 +1,7 @@
 const konamiCode = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown",	"ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"];
 
 let konamiIndex = 0;
+let konami = false;
 
 document.addEventListener('keydown', function checkKonamiCode(event) {
 	if (event.key === konamiCode[konamiIndex]) {
@@ -9,8 +10,18 @@ document.addEventListener('keydown', function checkKonamiCode(event) {
 			document.body.style.backgroundImage = 'url("/public/img/background/wait.png")';
 			document.getElementById("navbarTitle").textContent = "LinkSupervisor";
 			konamiIndex = 0;
+			konami = true;
 		}
 	} else {
 		konamiIndex = 0;
+	}
+
+	if (event.key === " ") {
+		if (konami == true) {
+			let rdm = Math.floor(Math.random() * 3)
+			let audio = new Audio("/public/sound/jump" + (rdm +1) + ".mp3")
+			audio.volume = 0.2;
+			audio.play()
+		}
 	}
 });
