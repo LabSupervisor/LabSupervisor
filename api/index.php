c6e94b224695a7244db57b77996f8162d1734ae9
marine404 <marine.gonnord.pro@gmail.com>
2024-06-03 11:50:30 +0200

    implements a functionality to dynamically update the selection menu on the user page according to user roles

--- api/index.php
+++ api/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use LabSupervisor\app\repository\ClassroomRepository;
 use
 	LabSupervisor\app\repository\UserRepository,
 	LabSupervisor\app\repository\SessionRepository,
@@ -117,6 +118,16 @@ switch($_SERVER["REQUEST_METHOD"]) {
 				// Answer API
 				echo '{"Response": {"Message": "Theme updated."}}';
 			}
+
+			// Application asking to update user theme
+			if ($data->ask == "get_teacher_classroom") {
+
+				$result = ClassroomRepository::getTeacherClassroom($data->idUser);
+
+				// Answer API
+				echo json_encode($result);
+			}
+
 		} catch (Exception $e) {
 			LogRepository::fileSave($e);
 		}
--- public/js/function/updateUser.js
+++ public/js/function/updateUser.js
@@ -1,4 +1,6 @@
 function updateUser(userId, classroomIdUser, roleIdUser) {
+	var currentClassId ;
+
 	var form = document.getElementById("form");
 	var surnameElement = document.getElementById("surname_" + userId);
 	var nameElement = document.getElementById("name_" + userId);
@@ -31,6 +33,11 @@ function updateUser(userId, classroomIdUser, roleIdUser) {
 	inputName.setAttribute("placeholder", lang("MAIN_NAME"));
 	inputName.setAttribute("value", name);
 
+	// Create a span element for "Aucune classe"
+	var noClassSpan = document.createElement("span");
+	noClassSpan.textContent = lang("USER_UPDATE_CLASS_EMPTY");
+	noClassSpan.setAttribute("id", "noClass");
+
 	var selectClassroom = document.createElement("select");
 	selectClassroom.setAttribute("id", "classroom");
 	selectClassroom.setAttribute("name", "classroom_" + userId);
@@ -40,7 +47,7 @@ function updateUser(userId, classroomIdUser, roleIdUser) {
 	optionNone.value = "0";
 
 	// Add the "no class" option to the top of the drop-down menu
-	selectClassroom.insertBefore(optionNone, selectClassroom.firstChild);
+	selectClassroom.appendChild(optionNone);
 
 	var selectRole = document.createElement("select");
 	selectRole.setAttribute("id", "role");
@@ -50,15 +57,16 @@ function updateUser(userId, classroomIdUser, roleIdUser) {
 	// Add options to select menu
 	for (var i = 0; i < classrooms.length; i++) {
 		var option = document.createElement("option");
-		// Use "name" property
 		option.text = classrooms[i].name;
 		option.value = classrooms[i].id;
-		if (classrooms[i].id == classroomIdUser) {
-			// Select option for actual classroom
+		if (roleIdUser != 3 && classrooms[i].id == classroomIdUser) {
+			// Select option for actual classroom, but not for professor
 			option.selected = true;
 		}
 		selectClassroom.add(option);
 	}
+
+	currentClassId = classroomIdUser;
 
 	// Add options to select menu
 	for (var i = 0; i < roles.length; i++) {
@@ -73,27 +81,117 @@ function updateUser(userId, classroomIdUser, roleIdUser) {
 		selectRole.add(option);
 	}
 
-	// Create a span element for "Aucune classe"
-	var noClassSpan = document.createElement("span");
-	noClassSpan.textContent = lang("USER_UPDATE_CLASS_EMPTY");
-	noClassSpan.setAttribute("id", "noClass");
+	if (roleIdUser == 3) {
+		fetch("/connect", {
+			method: 'post',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: '{"ask": "get_teacher_classroom", "idUser":' + userId + '}'
+		}).then((response) => {
+			return response.json()
+		}).then((classroomTeacher) => {
+			// Set multiple and required attributes
+			selectClassroom.setAttribute("multiple", "multiple");
+			selectClassroom.setAttribute("required", "required");
 
-    // Add event listener to change the classroom select element based on role
-    selectRole.addEventListener('change', function() {
-        if (selectRole.value == '3') {
-            selectClassroom.setAttribute("multiple", "multiple");
-            classRoomElement.replaceChildren(selectClassroom);
-        } else if (selectRole.value == '1') {
-            classRoomElement.replaceChildren(noClassSpan);
-        } else {
-            selectClassroom.removeAttribute("multiple");
-            classRoomElement.replaceChildren(selectClassroom);
-        }
+			// Deselect all options first
+			for (var i = 0; i < selectClassroom.options.length; i++) {
+				selectClassroom.options[i].selected = false;
+			}
+
+			// Select the classes for the teacher
+			classroomTeacher.forEach(function (classroom) {
+				for (var i = 0; i < selectClassroom.options.length; i++) {
+					if (selectClassroom.options[i].value == classroom.id) {
+						selectClassroom.options[i].selected = true;
+					}
+				}
+			});
+		}).catch((error) => {
+			console.log(error)
+		})
+	}
+
+	// Add event listener to change the classroom select element based on role
+	selectRole.addEventListener('change', function() {
+		if (selectRole.value == '3') {
+			selectClassroom.setAttribute("multiple", "multiple");
+			selectClassroom.setAttribute("required", "required");
+			// Deselect all other options
+			for (var i = 0; i < selectClassroom.options.length; i++) {
+				selectClassroom.options[i].selected = false;
+			}
+			fetch("/connect", {
+				method: 'post',
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json'
+				},
+				body: '{"ask": "get_teacher_classroom", "idUser":' + userId + '}'
+			}).then((response) => {
+				return response.json()
+			}).then((classroomTeacher) => {
+				// Set multiple and required attributes
+				selectClassroom.setAttribute("multiple", "multiple");
+				selectClassroom.setAttribute("required", "required");
+
+				if (classroomTeacher.length == 0) {
+					optionNone.selected = true; // Select "Aucune classe"
+				} else {
+					// Deselect all options first
+					for (var i = 0; i < selectClassroom.options.length; i++) {
+						selectClassroom.options[i].selected = false;
+					}
+
+					// Select the classes for the teacher
+					classroomTeacher.forEach(function (classroom) {
+						for (var i = 0; i < selectClassroom.options.length; i++) {
+							if (selectClassroom.options[i].value == classroom.id) {
+								selectClassroom.options[i].selected = true;
+							}
+						}
+					});
+				}
+			}).catch((error) => {
+				console.log(error)
+			})
+
+			classRoomElement.replaceChildren(selectClassroom);
+		} else if (selectRole.value == '1') {
+			classRoomElement.replaceChildren(noClassSpan);
+		} else if (selectRole.value == '2') {
+			// Remove multiple and required attributes
+			selectClassroom.removeAttribute("multiple");
+
+			if (currentClassId != 0) {
+				for (var i = 0; i < classrooms.length; i++) {
+					var option = document.createElement("option");
+					option.text = classrooms[i].name;
+					option.value = classrooms[i].id;
+					if (roleIdUser != 3 && classrooms[i].id == classroomIdUser) {
+						// Select option for actual classroom, but not for professor
+						option.selected = true;
+					}
+					selectClassroom.add(option);
+				}
+			} else {
+				optionNone.selected = true; // Select "Aucune classe"
+			}
+
+			classRoomElement.replaceChildren(selectClassroom);
+		}
 	});
+
+	if (roleIdUser == 1) {
+		classRoomElement.replaceChildren(noClassSpan);
+	} else {
+		classRoomElement.replaceChildren(selectClassroom);
+	}
 
 	surnameElement.replaceChildren(inputSurname);
 	nameElement.replaceChildren(inputName);
-	classRoomElement.replaceChildren(selectClassroom);
 	roleElement.replaceChildren(selectRole);
 
 	var inputUserId = document.createElement("input");
@@ -117,4 +215,3 @@ function updateUser(userId, classroomIdUser, roleIdUser) {
 
 	modifyButton.parentNode.replaceChild(confirmButton, modifyButton);
 }
-
