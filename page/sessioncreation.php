2d67aa54a7b3de5877e13dd509c54a87fda06de5
Romain Plantefève <romain.plantefeve@laposte.net>
2025-02-24 16:15:31 +0100

    Formatting sessioncreation

--- page/sessioncreation.php
+++ page/sessioncreation.php
@@ -2,221 +2,218 @@
 
 use LabSupervisor\app\repository\ClassroomRepository;
 use LabSupervisor\app\repository\SessionRepository;
-	use LabSupervisor\app\repository\UserRepository;
+use LabSupervisor\app\repository\UserRepository;
 
 use function LabSupervisor\functions\mainHeader;
 use function LabSupervisor\functions\lang;
 
-	// Logic
-require $_SERVER["DOCUMENT_ROOT"] . '/logic/createSession.php';
+// Logic
+require $_SERVER['DOCUMENT_ROOT'] . '/logic/createSession.php';
 
-	// Import header
-	mainHeader(lang("NAVBAR_CREATE_SESSION"), true);
+// Import header
+mainHeader(lang('NAVBAR_CREATE_SESSION'), true);
 
-	$idProv = 1;
+$idProv = 1;
 ?>
 
 <link rel="stylesheet" href="/public/css/form.css">
 
 <div class="mainbox mainform">
-	<a class="back" href="/sessions"><i class="ri-arrow-left-line"></i> <?= lang("MAIN_BUTTON_BACK") ?></a>
-	<form id="formSession" method="post" onsubmit="loading()">
-		<div class="row">
-			<div class="column">
-				<!-- Main informations -->
-				<div>
-					<h2><i class="ri-information-line"></i> <?= lang("SESSION_CREATE_TITLE_CHARACTERISTICS") ?></h2>
-				</div>
-				<div>
-                    <input type="text" placeholder="<?= lang("SESSION_CREATE_CHARACTERISTICS_TITLE") ?>" id="titleSession" name="titleSession" value="<?= isset($sessionData) === true ? $sessionData['title'] : "" ?>" required>
-				</div>
-				<div>
-                    <textarea placeholder="<?= lang("SESSION_CREATE_CHARACTERISTICS_CONTENT") ?>" id="descriptionSession" name="descriptionSession"><?= isset($sessionData) === true ? $sessionData['description'] : "" ?></textarea>
-				</div>
+    <a class="back" href="/sessions"><i class="ri-arrow-left-line"></i> <?= lang('MAIN_BUTTON_BACK'); ?></a>
+    <form id="formSession" method="post" onsubmit="loading()">
+        <div class="row">
+            <div class="column">
+                <!-- Main informations -->
+                <div>
+                    <h2><i class="ri-information-line"></i> <?= lang('SESSION_CREATE_TITLE_CHARACTERISTICS'); ?></h2>
+                </div>
+                <div>
+                    <input type="text" placeholder="<?= lang('SESSION_CREATE_CHARACTERISTICS_TITLE'); ?>" id="titleSession" name="titleSession" value="<?= isset($sessionData) === true ? $sessionData['title'] : ''; ?>" required>
+                </div>
+                <div>
+                    <textarea placeholder="<?= lang('SESSION_CREATE_CHARACTERISTICS_CONTENT'); ?>" id="descriptionSession" name="descriptionSession"><?= isset($sessionData) === true ? $sessionData['description'] : ''; ?></textarea>
+                </div>
 
-				<!-- Participants -->
-				<div>
-					<h2><i class="ri-group-line"></i> <?= lang("SESSION_CREATE_TITLE_PARTICIPANT") ?></h2>
-				</div>
-				<div>
-					<select id="classroomSelect" name="classes" onchange="updateClassroom(this.value)">
-					<?php
-						$classrooms = ClassroomRepository::getTeacherClassroom($_SESSION["login"]);
-						foreach ($classrooms as $value) {
-                        if ($value["active"] === 1) {
+                <!-- Participants -->
+                <div>
+                    <h2><i class="ri-group-line"></i> <?= lang('SESSION_CREATE_TITLE_PARTICIPANT'); ?></h2>
+                </div>
+                <div>
+                    <select id="classroomSelect" name="classes" onchange="updateClassroom(this.value)">
+                    <?php
+                    $classrooms = ClassroomRepository::getTeacherClassroom($_SESSION['login']);
+                    foreach ($classrooms as $value) {
+                        if ($value['active'] === 1) {
                             if (isset($_POST['sessionId']) === true) {
-                                if ($value["id"] === SessionRepository::getClassroom($_POST['sessionId'])) {
-										echo "<option selected='selected' value=" . $value["id"] . ">" . htmlspecialchars($value["name"]) . "</option>";
-									} else {
-										echo "<option value=" . $value["id"] . ">" . htmlspecialchars($value["name"]) . "</option>";
-									}
-								} else {
-									echo "<option value=" . $value["id"] . ">" . htmlspecialchars($value["name"]) . "</option>";
-								}
-							}
-						}
-					?>
-					</select>
-				</div>
-				<div>
+                                if ($value['id'] === SessionRepository::getClassroom($_POST['sessionId'])) {
+                                    echo "<option selected='selected' value=" . $value['id'] . '>' . htmlspecialchars($value['name']) . '</option>';
+                                } else {
+                                    echo '<option value=' . $value['id'] . '>' . htmlspecialchars($value['name']) . '</option>';
+                                }
+                            } else {
+                                echo '<option value=' . $value['id'] . '>' . htmlspecialchars($value['name']) . '</option>';
+                            }
+                        }
+                    }
+                    ?>
+                    </select>
+                </div>
+                <div>
                     <h2><i class="ri-group-line"></i> Professeurs </h2>
-				</div>
-				<div>
-					<?php
+                </div>
+                <div>
+                    <?php
                     if (isset($_POST['sessionId']) === true) {
-					?>
-						<div id="teachersession">
-							<?php
-							$teachers = SessionRepository::getTeacherParticipants($_POST['sessionId']);
+                        ?>
+                        <div id="teachersession">
+                        <?php
+                        $teachers = SessionRepository::getTeacherParticipants($_POST['sessionId']);
                         if (isset($teachers) === true) {
                             echo '<p>';
-								foreach ($teachers as $teacher) {
-									$infoUser = UserRepository::getInfo($teacher['iduser']);
-									echo "Nom prof: " . $infoUser['name'] . "</br>";
-								}
-							} else {
-								echo "Aucun autre professeur trouvé pour cette session.";
-							}
-							echo '<button type="button" class="button" onclick="updateTeacher()">+</button>' ;
-
-						} else {
-
-						}
-					?>
-						</div>
+                            foreach ($teachers as $teacher) {
+                                $infoUser = UserRepository::getInfo($teacher['iduser']);
+                                echo 'Nom prof: ' . $infoUser['name'] . '</br>';
+                            }
+                        } else {
+                            echo 'Aucun autre professeur trouvé pour cette session.';
+                        }
+                        echo '<button type="button" class="button" onclick="updateTeacher()">+</button>';
+                    }
+                    ?>
+                        </div>
                 <div>
-					<?php
+                    <?php
                     if (isset($_POST['sessionId']) === true) {
-							$idSession = $_POST['sessionId'];
-							$teachers = SessionRepository::getTeacherNotInSession($_POST['sessionId']);
-					?>
-							<select name="teacher[]" multiple required>
-								<?php foreach ($teachers as $teacher){ ?>
-									<option value="<?= $teacher['iduser']; ?>">
-										<?= $teacher['name'] . ' ' . $teacher['surname']; ?>
-									</option>
-								<?php } ?>
-							</select>
+                        $idSession = $_POST['sessionId'];
+                        $teachers = SessionRepository::getTeacherNotInSession($_POST['sessionId']);
+                        ?>
+                            <select name="teacher[]" multiple required>
+                                <?php foreach ($teachers as $teacher) { ?>
+                                    <option value="<?= $teacher['iduser']; ?>">
+                                        <?= $teacher['name'] . ' ' . $teacher['surname']; ?>
+                                    </option>
+                                <?php } ?>
+                            </select>
                             </div>
-						<?php
-						}
-						?>
-			</div>
+                        <?php
+                    }
+                    ?>
             </div>
-			<!-- Chapters -->
-			<div class="column">
-				<div>
-					<h2><i class="ri-bookmark-line"></i> <?= lang("SESSION_CREATE_TITLE_CHAPTER") ?></h2>
-				</div>
+            </div>
+            <!-- Chapters -->
+            <div class="column">
+                <div>
+                    <h2><i class="ri-bookmark-line"></i> <?= lang('SESSION_CREATE_TITLE_CHAPTER'); ?></h2>
+                </div>
 
-				<div id="fieldsContainer">
-					<?php
-					$nbChapter = 1;
+                <div id="fieldsContainer">
+                    <?php
+                    $nbChapter = 1;
 
-					// Check session exist
+                    // Check session exist
                     if (isset($_POST['sessionId']) === true) {
-						$tabChapter = SessionRepository::getActiveChapter($_POST['sessionId']);
+                        $tabChapter = SessionRepository::getActiveChapter($_POST['sessionId']);
 
-						// Print field exist chapter
-						foreach ($tabChapter as $i => $chapter) {
-					?>
-					<div class="subform" id="<?= $chapter["id"] ?>">
+                        // Print field exist chapter
+                        foreach ($tabChapter as $i => $chapter) {
+                            ?>
+                            <div class="subform" id="<?= $chapter['id']; ?>">
 
-						<!-- id chapter -->
-                                <input type="hidden" class="chapterId" id="idChapter<?= $chapter["id"] ?>" value="<?= $chapter["id"] ?>">
+                                <!-- id chapter -->
+                                        <input type="hidden" class="chapterId" id="idChapter<?= $chapter['id']; ?>" value="<?= $chapter['id']; ?>">
 
-						<input placeholder="<?= lang("SESSION_CREATE_CHAPTER_TITLE") ?>" type="text" id="titleChapter<?= $chapter["id"] ?>" value="<?= $chapter["title"] ?>" onchange="updateChapter(this.parentNode.id)" required>
+                                <input placeholder="<?= lang('SESSION_CREATE_CHAPTER_TITLE'); ?>" type="text" id="titleChapter<?= $chapter['id']; ?>" value="<?= $chapter['title']; ?>" onchange="updateChapter(this.parentNode.id)" required>
 
-                                <textarea placeholder="<?= lang("SESSION_CREATE_CHAPITRE_CONTENT") ?>" id="chapterDescription<?= $chapter["id"] ?>" onchange="updateChapter(this.parentNode.id)" ><?= $chapter["description"] ?></textarea>
+                                        <textarea placeholder="<?= lang('SESSION_CREATE_CHAPITRE_CONTENT'); ?>" id="chapterDescription<?= $chapter['id']; ?>" onchange="updateChapter(this.parentNode.id)" ><?= $chapter['description']; ?></textarea>
 
-						<!-- Delete chapter button -->
-						<button type="button" class="button chapterButton" onclick="deleteChapter(this)"><?= lang("SESSION_CREATE_CHAPTER_REMOVE") ?></button>
-					</div>
-					<?php
-						}
-						//create session
-					} else {
-					?>
-					<!-- <div id="fieldsContainer"> -->
-					<div class="subform">
-						<input type="text" placeholder="<?= lang("SESSION_CREATE_CHAPTER_TITLE") ?>" name="addChapters[0][title]" class="field">
-						<textarea name="addChapters[0][desc]" placeholder="<?= lang("SESSION_CREATE_CHAPITRE_CONTENT") ?>" class="field"></textarea>
+                                <!-- Delete chapter button -->
+                                <button type="button" class="button chapterButton" onclick="deleteChapter(this)"><?= lang('SESSION_CREATE_CHAPTER_REMOVE'); ?></button>
+                            </div>
+                            <?php
+                        }
+                        // create session
+                    } else {
+                        ?>
+                    <!-- <div id="fieldsContainer"> -->
+                    <div class="subform">
+                        <input type="text" placeholder="<?= lang('SESSION_CREATE_CHAPTER_TITLE'); ?>" name="addChapters[0][title]" class="field">
+                        <textarea name="addChapters[0][desc]" placeholder="<?= lang('SESSION_CREATE_CHAPITRE_CONTENT'); ?>" class="field"></textarea>
 
-						<!-- Delete chapter button -->
-						<button type="button" class="button chapterButton" onclick="deleteChapter(this)"><?= lang("SESSION_CREATE_CHAPTER_REMOVE") ?></button>
-					</div>
+                        <!-- Delete chapter button -->
+                        <button type="button" class="button chapterButton" onclick="deleteChapter(this)"><?= lang('SESSION_CREATE_CHAPTER_REMOVE'); ?></button>
+                    </div>
                         <?php
                     }
                     ?>
 
-					<!-- Field allowing you to keep the number of chapters, updated by the js, sent to the form for chapter management -->
-					<div>
-						<input type="hidden" value="<?= $nbChapter ?>" name="nbChapter" id="nbChapter">
-					</div>
+                    <!-- Field allowing you to keep the number of chapters, updated by the js, sent to the form for chapter management -->
+                    <div>
+                        <input type="hidden" value="<?= $nbChapter; ?>" name="nbChapter" id="nbChapter">
+                    </div>
 
-					<!-- Add chapter button -->
-					<div>
-                        <button class="button" type="button" id="btn-chapter" data-id="1" onclick="addHTMLChapter('<?= lang("SESSION_CREATE_CHAPTER_TITLE") ?>', '<?= lang("SESSION_CREATE_CHAPITRE_CONTENT") ?>', this)"><?= lang("SESSION_CREATE_CHAPTER_ADD") ?></button>
-					</div>
+                    <!-- Add chapter button -->
+                    <div>
+                        <button class="button" type="button" id="btn-chapter" data-id="1" onclick="addHTMLChapter('<?= lang('SESSION_CREATE_CHAPTER_TITLE'); ?>', '<?= lang('SESSION_CREATE_CHAPITRE_CONTENT'); ?>', this)"><?= lang('SESSION_CREATE_CHAPTER_ADD'); ?></button>
+                    </div>
 
-				</div>
-			</div>
+                </div>
+            </div>
 
-			<div class="column">
-				<!-- Date -->
-				<div>
-					<h2><i class="ri-calendar-2-line"></i> <?= lang("SESSION_CREATE_TITLE_DATE") ?></h2>
-				</div>
-				<div>
-                    <input type="datetime-local" id="date" name="date" value="<?= isset($sessionData) === true ? $sessionData['date'] : "" ?>" required>
-				</div>
+            <div class="column">
+                <!-- Date -->
+                <div>
+                    <h2><i class="ri-calendar-2-line"></i> <?= lang('SESSION_CREATE_TITLE_DATE'); ?></h2>
+                </div>
+                <div>
+                    <input type="datetime-local" id="date" name="date" value="<?= isset($sessionData) === true ? $sessionData['date'] : ''; ?>" required>
+                </div>
 
-				<!-- State -->
-			<?php
-				$checked = "";
-                if (isset($_POST['sessionId']) === true) {
-                    if (SessionRepository::getState($_POST['sessionId']) !== 0) {
-						$checked = "checked";
-					}
-				}
-			?>
-				<div>
-					<h2><i class="ri-door-open-line"></i> <?= lang("SESSION_CREATE_STATE") ?></h2>
-				</div>
-				<div>
-					<label class="checkboxContainer"><?= lang("SESSION_CREATE_STATE_OPEN") ?>
-						<input class="checkbox" type="checkbox" name="state" <?= $checked ?>>
-						<span class="checkmark"></span>
-					</label>
-				</div>
-			</div>
-		</div>
+                <!-- State -->
+            <?php
+            $checked = '';
+            if (isset($_POST['sessionId']) === true) {
+                if (SessionRepository::getState($_POST['sessionId']) !== 0) {
+                    $checked = 'checked';
+                }
+            }
+            ?>
+                <div>
+                    <h2><i class="ri-door-open-line"></i> <?= lang('SESSION_CREATE_STATE'); ?></h2>
+                </div>
+                <div>
+                    <label class="checkboxContainer"><?= lang('SESSION_CREATE_STATE_OPEN'); ?>
+                        <input class="checkbox" type="checkbox" name="state" <?= $checked; ?>>
+                        <span class="checkmark"></span>
+                    </label>
+                </div>
+            </div>
+        </div>
 
-		<!-- Send -->
-		<?php
+        <!-- Send -->
+        <?php
         if (isset($_POST['sessionId']) === true) {
-		?>
-		<div>
-                <input type="hidden" name="idSession" value="<?= $_POST['sessionId'] ?>" >
-			<button type="submit" name="updateSession" class="button save"><i class="ri-loop-left-line"></i> <?= lang("MAIN_SAVE") ?></button>
-		</div>
-		<?php
-			} else {
-		?>
-		<div>
-			<button class="button" type="submit" name="saveSession"><i class="ri-save-2-line"></i> <?= lang("MAIN_SAVE") ?></button>
-		</div>
-		<?php
-			}
-		?>
-	</form>
+            ?>
+        <div>
+                <input type="hidden" name="idSession" value="<?= $_POST['sessionId']; ?>" >
+            <button type="submit" name="updateSession" class="button save"><i class="ri-loop-left-line"></i> <?= lang('MAIN_SAVE'); ?></button>
+        </div>
+            <?php
+        } else {
+            ?>
+        <div>
+            <button class="button" type="submit" name="saveSession"><i class="ri-save-2-line"></i> <?= lang('MAIN_SAVE'); ?></button>
+        </div>
+            <?php
+        }
+        ?>
+    </form>
 
-	<?php
+    <?php
     if (isset($_POST['sessionId']) === true) {
-	?>
-	<form method="POST" onsubmit="return confirmForm('<?= lang('SESSION_CREATE_DELETE_CONFIRMATION') ?>');">
-		<button class="link" type="submit" name="deleteSession" value="<?= $_POST['sessionId'] ?>"><i class="ri-delete-bin-line"></i> <?= lang("SESSION_CREATE_DELETE") ?></button>
-	</form>
+        ?>
+    <form method="POST" onsubmit="return confirmForm('<?= lang('SESSION_CREATE_DELETE_CONFIRMATION'); ?>');">
+        <button class="link" type="submit" name="deleteSession" value="<?= $_POST['sessionId']; ?>"><i class="ri-delete-bin-line"></i> <?= lang('SESSION_CREATE_DELETE'); ?></button>
+    </form>
 
         <?php
     }
@@ -224,7 +221,7 @@ require $_SERVER["DOCUMENT_ROOT"] . '/logic/createSession.php';
 </div>
 
 <script>
-	var nbChapter = 1;
+    var nbChapter = 1;
 </script>
 
 <script src="/public/js/function/updateTeacher.js"></script>
@@ -237,8 +234,8 @@ require $_SERVER["DOCUMENT_ROOT"] . '/logic/createSession.php';
 <script src="/public/js/function/popupConfirm.js"></script>
 
 <?php
-if (isset($_POST["titleSession"]) === true) {
+if (isset($_POST['titleSession']) === true) {
     echo '<script> popupDisplay("' . lang('SESSION_CREATE_UPDATE_NOTIFICATION') . '"); </script>';
-	}
+}
 
-require $_SERVER["DOCUMENT_ROOT"] . '/include/footer.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/include/footer.php';
